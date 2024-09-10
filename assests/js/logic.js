5b51b2743d489735775bff5aa14e45c257805039
Erin DeVine <erinj291@gmail.com>
2024-09-09 21:24:59 -0400

    app working. Need to fix styling

--- assests/js/logic.js
+++ assests/js/logic.js
@@ -2,6 +2,10 @@
 // API key for fetch calls
 const apiKey = 'b151c1f3e021551c216fcf9e899287cc';
 
+const logButtonsContainer= document.getElementById('city-buttons-container');
+const todayForecastContainer = document.getElementById('main-card');
+const fiveDayContainer = document.getElementById('five-day');
+
 // Create array to store the search history buttons for cities entered by user (obtained from local storage)
 let cityList = JSON.parse(localStorage.getItem("City Input")) || [];
 
@@ -10,116 +14,165 @@ let cityList = JSON.parse(localStorage.getItem("City Input")) || [];
 const searchBar = document.getElementById('search-bar');
 const searchBtn = document.getElementById('search-submit-btn');
 
+
 searchBtn.addEventListener('click', function (event) {
 
 event.preventDefault();
 const cityInput = searchBar.value.trim();
 
-// Fetch request to obtain lat/lon info for city based on name entered by user 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`)
-.then(response => {
-    if (!response.ok) {
-      throw new Error('Not able to fetch data.');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
+if (!cityInput) {
+    alert("Please enter the name of a city you'd like to search for!")
+} else {
+  logNewCity(cityInput);
+  saveCityToHistory(cityInput);
+  getCurrentWeather(cityInput);
+  getFiveForecast(cityInput);
 
-    // add fetched data to array of objects set to local storage
-    cityList.push({city: cityInput, lat: data.coord.lat, lon: data.coord.lon});
-    localStorage.setItem("City Input", JSON.stringify(cityList));
+}
+});
 
-    // Append contents of array to the page to display as buttons
-    const cityLog = document.getElementById('city-buttons-container');
-    const searchedCity = document.createElement('button');
-    searchedCity.classList.add('log-btns')
-    searchedCity.textContent = cityInput;
-    cityLog.append(searchedCity);
-
-
-// Fetch request to retrieve and show forecast info based on most recent searched city. 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityList[cityList.length -1].lat}&lon=${cityList[cityList.length -1].lon}&appid=${apiKey}`)
-
-.then(response => {
-  if (!response.ok) {
-    throw new Error('Not able to fetch data.');
-  }
-  return response.json();
-})
-.then(currentData => {
-  console.log(currentData);
-
-// Set data to each element of main card based on fetch content
-const date = dayjs(currentData.list[0].dt_txt).format('M/D/YYYY');
-const temperature = currentData.list[0].main.temp;
-const wind = currentData.list[0].wind.speed;
-const humidity = currentData.list[0].main.humidity;
-const weatherEmoji = currentData.list[0].weather[0].icon;
-
-// Reference the container for current forecase
-const todayForecast = document.getElementById('main-card');
-
-// Add text content for each element of main card forecast
-const mainHeader = document.createElement('h4');
-mainHeader.textContent = `${cityInput} (${date}) ${weatherEmoji}`;
-
-const mainTemp = document.createElement('p');
-mainTemp.textContent = `Temp: ${temperature} °F`;
-
-const mainWind = document.createElement('p');
-mainWind.textContent = `Wind: ${wind} mph`;
-
-const mainHumidity = document.createElement('p');
-mainHumidity.textContent = `Humidity: ${humidity} %`;
-
-// Append all new elements to the main card container
-todayForecast.append(mainHeader);
-todayForecast.append(mainTemp);
-todayForecast.append(mainWind);
-todayForecast.append(mainHumidity);
-todayForecast.style.display = 'block';
-
-// Loop through the data and INCREMENT based on 8 given how data is returned based on 3 hour increments for each date (every 8th index will be a new day)
-for (let i = 8; i < currentData.list.length; i += 8) {
-
-    const forecastIndex = (i - 8) / 8; 
-
-  const fiveDayCards = document.getElementsByClassName('day-cards')[forecastIndex];
+function logNewCity (cityInput) {
   
+  const cityBtn = document.createElement('button');
+  cityBtn.classList.add('log-btns');
+  cityBtn.textContent = cityInput;
 
-  const fiveDate = document.createElement('h5');
-  fiveDate.textContent = dayjs(currentData.list[i].dt_txt).format('M/D/YYYY');
+  cityBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    getCurrentWeather(cityInput);
+    getFiveForecast(cityInput);
 
-  const fiveEmoji = document.createElement('img');
-  fiveEmoji.src = `http://openweathermap.org/img/wn/${currentData.list[i].weather[0].icon}.png`
+  });
 
-  const fiveTemp = document.createElement('p');
-  fiveTemp.textContent = `Temp: ${currentData.list[i].main.temp} °F`;
+  logButtonsContainer.append(cityBtn);
 
-  const fiveWind = document.createElement('p');
-  fiveWind.textContent = `Wind: ${currentData.list[i].wind.speed} mph`;
-
-  const fiveHumid = document.createElement('p');
-  fiveHumid.textContent = `Humidity: ${currentData.list[i].main.humidity} %`;
-
-
-
-  fiveDayCards.append(fiveDate);
-  fiveDayCards.append(fiveEmoji);
-  fiveDayCards.append(fiveTemp);
-  fiveDayCards.append(fiveWind);
-  fiveDayCards.append(fiveHumid);
-
-  fiveDayCards.style.display = 'block';
 }
 
+
+function saveCityToHistory(cityInput) {
+  if (!cityList.includes(cityInput)) {
+    cityList.push(cityInput);
+    localStorage.setItem('cityList', JSON.stringify(cityList));
+  }
+};
+
+
+function getAllCities () {
+  const allCities = JSON.parse(localStorage.getItem('cityList')) || [];
+  allCities.forEach(function(cityInput) {
+    logNewCity(cityInput);
+  })
+};
+
+document.addEventListener('DOMContentLoaded', getAllCities);
+
+
+function getCurrentWeather (cityInput) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`, {
 })
-.catch(error => {
-  console.log(error);
-  });
-})
-.catch(error => {
-  console.log(error);
-  });
-});
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let currentWeather = {
+      cityName: data.name,
+      todayDate: dayjs().format('M/D/YYYY'),
+      todayTemp: data.main.temp,
+      todayWind: data.wind.speed,
+      todayHumid: data.main.humidity,
+      todayEmoji: data.weather[0].icon
+    }
+
+    createMainCard(currentWeather);
+
+  })
+};
+
+
+function getFiveForecast(cityInput) {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast/?q=${cityInput}&appid=${apiKey}`, {
+
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // Clear our any prior data first
+    fiveDayContainer.innerHTML = '';
+    for (let i = 0; i <= 32; i += 8) {
+      let fiveDayWeather = {
+          date: dayjs(data.list[i].dt_txt).format('M/D/YYYY'),
+          temp: data.list[i].main.temp,
+          humidity: data.list[i].main.humidity,
+          wind: data.list[i].wind.speed,
+          icon: data.list[i].weather[0].icon
+      }
+
+      createFiveDayCards(fiveDayWeather);
+    }
+  })
+};
+
+
+function createMainCard (currentWeather) {
+// Clear any prior data first
+  todayForecastContainer.innerHTML = '';
+
+  if (!currentWeather || !currentWeather.todayEmoji) {
+    console.error("Current weather data not available");
+    return;
+};
+
+  // Add text content for each element of main card forecast
+  const todayEmoji = document.createElement('img');
+  todayEmoji.src = `https://openweathermap.org/img/w/${currentWeather.todayEmoji}.png`;
+  todayEmoji.alt = "Weather Icon";
+
+  const mainHeaderEl = document.createElement('h4');
+  mainHeaderEl.textContent = `${currentWeather.cityName} (${currentWeather.todayDate})`;
+  mainHeaderEl.appendChild(todayEmoji);
+
+  const mainTempEl = document.createElement('p');
+  mainTempEl.textContent = `Temp: ${currentWeather.todayTemp} °F`;
+  
+  const mainWindEl = document.createElement('p');
+  mainWindEl.textContent = `Wind: ${currentWeather.todayWind} mph`;
+  
+  const mainHumidityEl = document.createElement('p');
+  mainHumidityEl.textContent = `Humidity: ${currentWeather.todayHumid} %`;
+
+  // Append all elements to the main weather display container (today forecast)
+  todayForecastContainer.append(mainHeaderEl);
+  todayForecastContainer.append(mainTempEl);
+  todayForecastContainer.append(mainWindEl);
+  todayForecastContainer.append(mainHumidityEl);
+  todayForecastContainer.style.display = 'block';
+
+};
+
+function createFiveDayCards (fiveDayWeather) {
+  const fiveCards = document.createElement('div');
+  fiveCards.classList.add('day-cards');
+  const fiveDateEl = document.createElement('h5');
+  fiveDateEl.textContent = fiveDayWeather.date;
+
+  const fiveEmojiEl = document.createElement('img');
+  fiveEmojiEl.src = `http://openweathermap.org/img/wn/${fiveDayWeather.icon}.png`;
+
+  const fiveTempEl = document.createElement('p');
+  fiveTempEl.textContent = `Temp: ${fiveDayWeather.temp} °F`;
+  const fiveWindEl = document.createElement('p');
+  fiveWindEl.textContent = `Wind: ${fiveDayWeather.wind} mph`;
+  const fiveHumidEl = document.createElement('p');
+  fiveHumidEl.textContent = `Humidity: ${fiveDayWeather.humidity} %`;
+
+  fiveCards.append(fiveDateEl);
+  fiveCards.append(fiveEmojiEl);
+  fiveCards.append(fiveTempEl);
+  fiveCards.append(fiveWindEl);
+  fiveCards.append(fiveHumidEl);
+  
+  fiveDayContainer.append(fiveCards);
+  fiveDayContainer.style.display = 'block';
+
+};
